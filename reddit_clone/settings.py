70b85e0ff30358554a4589361c3cd8be6bdbad9a
Murithi Mwitharii <brodiamyzalius@gmail.com>
2021-06-23 16:04:03 +0300

    hosting

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if config('KEY'):
-    SECRET_KEY = config('KEY')
-else:
-    SECRET_KEY = 'django-insecure-wf*i)x^#*+f7+r!al*q&qqi&jyww7vi@$*^%7u8xivhwkzv!u@'
+# if config('KEY'):
+#     SECRET_KEY = config('KEY')
+# else:
+SECRET_KEY = 'django-insecure-wf*i)x^#*+f7+r!al*q&qqi&jyww7vi@$*^%7u8xivhwkzv!u@'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
