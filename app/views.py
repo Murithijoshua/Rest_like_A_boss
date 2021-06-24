6cff3d1afcfba7c7ff65183dc459bfbb145be736
Murithi Mwitharii <brodiamyzalius@gmail.com>
2021-06-24 15:42:19 +0300

    able to delete a post

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions, mixins,status
+from rest_framework import generics, permissions, mixins, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from . models import Post, Vote
@@ -9,7 +9,7 @@ from .serializers import PostSerializer, VoteSerializer
 class PostList(generics.ListCreateAPIView):
     """
     sorts out GET,POST,PUT 
-    
+
     """
     # getting all the data as python objects
     queryset = Post.objects.all()
@@ -18,28 +18,44 @@ class PostList(generics.ListCreateAPIView):
     # authentication_classes where if autheticated you can send post request else readonly
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     # saving data on post
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    """
+    sorts out GET,POST,PUT 
+
+    """
+    # getting all the data as python objects
+    queryset = Post.objects.all()
+    # serializing the Data
+    serializer_class = PostSerializer
+    # authentication_classes where if autheticated you can send post request else readonly
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+     
 class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # geting the users,post 
+    # geting the users,post
+
     def get_queryset(self):
         user = self.request.user
         post = Post.objects.get(pk=self.kwargs['pk'])
         return Vote.objects.filter(voter=user, post=post)
     # performing vote create to (pk) vote
+
     def perform_create(self, serializer):
         if self.get_queryset().exists():
             raise ValidationError("You have already voted for this post")
-        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
-    
-    
-    def delete(self,request,*args,**kwargs):
+        serializer.save(voter=self.request.user,
+                        post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
         if self.get_queryset().exists():
             self.get_queryset().delete()
-            return Response(status = status.HTTP_204_NO_CONTENT) 
+            return Response(status=status.HTTP_204_NO_CONTENT)
         else:
-            raise ValidationError("You didnt voted for this Silly!!") 
+            raise ValidationError("You didnt voted for this Silly!!")
--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wf*i)x^#*+f7+r!al*q&qqi&jyww7vi@$*^%7u8xivhwkzv!u@
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
